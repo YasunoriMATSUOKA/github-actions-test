438fb24b8feaf3b86ccc4f674108ce7d1b7ed6bf
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-12-25 16:15:12 +0900

    feat: schedule function to backup auth to storage pubsub

---
+++ functions/src/v1/pubsub/schedule/backupAuthToStorage.ts
@@ -0,0 +1,65 @@
+import * as fs from 'fs';
+import functions from '../../../utils/firebase/baseFunction';
+import { logger } from '../../../utils/firebase/logger';
+import { storage } from '../../../utils/firebase';
+
+const firebaseTools = require('firebase-tools');
+
+export const backupAuthToStorage = () =>
+  functions()
+    .runWith({ memory: '1GB' })
+    .pubsub.schedule('0 3 * * *')
+    .timeZone('Asia/Tokyo')
+    .onRun(async () => {
+      try {
+        const projectID =
+          process.env.GCLOUD_PROJECT || process.env.GCLOUD_PROJECT;
+        if (!projectID) {
+          throw Error('No project ID found');
+        }
+
+        const bucketName = `gs://${projectID}-auth-backups`;
+
+        logger.debug(`Backing up ${projectID} auth to ${bucketName}`);
+
+        const now = new Date();
+        const timestamp =
+          now.getFullYear() +
+          ('0' + (now.getMonth() + 1)).slice(-2) +
+          ('0' + now.getDate()).slice(-2) +
+          '-' +
+          ('0' + now.getHours()).slice(-2) +
+          ('0' + now.getMinutes()).slice(-2) +
+          ('0' + now.getSeconds()).slice(-2);
+        logger.debug('timestamp', { timestamp });
+
+        const tmpDir = '/tmp';
+        const plainJsonFileName = `auth-backup-${timestamp}.json`;
+        logger.debug('plainJsonFileName', { plainJsonFileName });
+        const plainJsonFilePath = `${tmpDir}/${plainJsonFileName}`;
+        logger.debug('plainJsonFilePath', { plainJsonFilePath });
+
+        logger.debug(`Downloading auth backup to ${plainJsonFilePath}...`);
+        await firebaseTools.auth.export(plainJsonFilePath, {
+          project: projectID,
+        });
+        logger.debug('Download complete');
+
+        logger.debug(`Uploading ${plainJsonFilePath} to ${bucketName}...`);
+        const bucket = storage.bucket(bucketName);
+        const gcsDestination = `${now.getFullYear()}/${(
+          '0' +
+          (now.getMonth() + 1)
+        ).slice(-2)}/${plainJsonFileName}`;
+        await bucket.upload(plainJsonFilePath, { destination: gcsDestination });
+        logger.debug('Upload complete');
+
+        logger.debug(`Deleting ${plainJsonFilePath}...`);
+        fs.unlinkSync(plainJsonFilePath);
+        logger.debug('Delete complete');
+
+        logger.debug('Backup operation complete');
+      } catch (err) {
+        logger.warn('Backup operation failed', { err });
+      }
+    });
--- functions/src/v1/pubsub/schedule/index.ts
+++ functions/src/v1/pubsub/schedule/index.ts
@@ -1,10 +1,12 @@
 import { CloudFunction } from 'firebase-functions';
 import { exportFunction } from '../../../utils/firebase/deploy';
 import { hello } from './hello';
+import { backupAuthToStorage } from './backupAuthToStorage';
 import { backupFirestoreToStorage } from './backupFirestoreToStorage';
 
 const _exportFunction = (name: string, f: () => CloudFunction<unknown>) =>
   exportFunction(['v1', 'pubsub', 'schedule', name], exports, f);
 
 _exportFunction('hello', hello);
+_exportFunction('backupAuthToStorage', backupAuthToStorage);
 _exportFunction('backupFirestoreToStorage', backupFirestoreToStorage);
