633920898f5bb040c578904ecae4debb29f1abaa
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-01-11 10:21:29 +0900

    feat: cypress e2e

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: '9fp137',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
